05359bd1813f01f780c234f7b1b952742575c7f7
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-05-14 19:55:28 +0400

    написал без цикла.

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -10,5 +10,16 @@
 #   Студенту надо попросить ХХХ.ХХ рублей
 
 educational_grant, expenses = 10000, 12000
-
-# TODO здесь ваш код
+expenses_1 = expenses + expenses * 0.03
+expenses_2 = expenses + expenses_1 * 0.03
+expenses_3 = expenses + expenses_2 * 0.03
+expenses_4 = expenses + expenses_3 * 0.03
+expenses_5 = expenses + expenses_4 * 0.03
+expenses_6 = expenses + expenses_5 * 0.03
+expenses_7 = expenses + expenses_6 * 0.03
+expenses_8 = expenses + expenses_7 * 0.03
+expenses_9 = expenses + expenses_8 * 0.03
+total_expenses = expenses + expenses_1 + expenses_2 + expenses_3 + expenses_4 + expenses_5 + expenses_6 + expenses_7 + expenses_8 + expenses_9
+total_education = educational_grant * 10
+money = total_expenses - total_education
+print('Студенту надо поросить', round(money,2), 'рублей')
