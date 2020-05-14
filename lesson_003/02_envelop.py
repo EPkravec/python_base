b9f5921e03d1eefbf1e788f4ceb7e2e59c251dde
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-05-14 17:33:11 +0400

    сделал 1 простое задание

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -11,15 +11,28 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+# paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
-# paper_x, paper_y = 6, 8
-# paper_x, paper_y = 8, 6
-# paper_x, paper_y = 3, 4
+# paper_x, paper_y = 6, 8  # ответ подходит
+# paper_x, paper_y = 8, 6  # ответ подходит
+# paper_x, paper_y = 3, 4  # ответ подходит
 # paper_x, paper_y = 11, 9
 # paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
+
+if envelop_x >= paper_x and envelop_y >= paper_y:
+    print('ДА')
+else:
+    if envelop_x >= paper_y and envelop_y >= paper_x:
+        print('ДА')
+    else:
+        print('НЕТ')
+
+
+
+
+
 
 # TODO здесь ваш код
 
