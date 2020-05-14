f8301ad8ee5afa6c2d5e309c167e28607069775d
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-05-14 17:51:01 +0400

    выполнил 2 более сложнее задачу

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -10,7 +10,7 @@
 # Результат проверки вывести на консоль (ДА/НЕТ)
 # Использовать только операторы if/elif/else, можно вложенные
 
-envelop_x, envelop_y = 10, 7
+# envelop_x, envelop_y = 10, 7
 # paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
@@ -48,18 +48,31 @@ else:
 # brick_x, brick_y, brick_z = 10, 2, 11
 # brick_x, brick_y, brick_z = 2, 10, 11
 # brick_x, brick_y, brick_z = 2, 11, 10
-# brick_x, brick_y, brick_z = 3, 5, 6
-# brick_x, brick_y, brick_z = 3, 6, 5
-# brick_x, brick_y, brick_z = 6, 3, 5
-# brick_x, brick_y, brick_z = 6, 5, 3
-# brick_x, brick_y, brick_z = 5, 6, 3
-# brick_x, brick_y, brick_z = 5, 3, 6
-# brick_x, brick_y, brick_z = 11, 3, 6
-# brick_x, brick_y, brick_z = 11, 6, 3
-# brick_x, brick_y, brick_z = 6, 11, 3
-# brick_x, brick_y, brick_z = 6, 3, 11
-# brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
+# brick_x, brick_y, brick_z = 3, 5, 6  # ответ подходит
+# brick_x, brick_y, brick_z = 3, 6, 5  # ответ подходит
+# brick_x, brick_y, brick_z = 6, 3, 5  # ответ подходит
+# brick_x, brick_y, brick_z = 6, 5, 3  # ответ подходит
+# brick_x, brick_y, brick_z = 5, 6, 3  # ответ подходит
+# brick_x, brick_y, brick_z = 5, 3, 6  # ответ подходит
+# brick_x, brick_y, brick_z = 11, 3, 6  # ответ подходит
+# brick_x, brick_y, brick_z = 11, 6, 3  # ответ подходит
+# brick_x, brick_y, brick_z = 6, 11, 3  # ответ подходит
+# brick_x, brick_y, brick_z = 6, 3, 11  # ответ подходит
+# brick_x, brick_y, brick_z = 3, 6, 11  # ответ подходит
+# brick_x, brick_y, brick_z = 3, 11, 6  # ответ подходит
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if hole_x >= brick_x and hole_y >= brick_y:
+    print('ДА')
+elif hole_x >= brick_y and hole_y >= brick_x:
+    print('ДА')
+elif hole_x >= brick_z and hole_y >= brick_x:
+    print('ДА')
+elif hole_x >= brick_x and hole_y >= brick_z:
+    print('ДА')
+elif hole_x >= brick_z and hole_y >= brick_y:
+    print('ДА')
+elif hole_x >= brick_y and hole_y >= brick_z:
+    print('ДА')
+else:
+    print('НЕТ')
