0186feb8e639096eb69e46ec47fbefb6c465996c
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-05-16 15:52:36 +0400

    ура получилось сделать линии в цикле, осталось разобраться с цветом

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -4,8 +4,26 @@
 
 import simple_draw as sd
 
-rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
-                  sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
+sd.resolution = (1200, 600)
+sd.background_color = (0, 0, 255)
+sd.caption = 'Радуга дуга 1 и 2 варианты'
+
+
+x, y = 50, 50
+x1, y1 = 350, 450
+
+start_point = sd.get_point(x, y)
+end_point = sd.get_point(x1, y1)
+sd.line(start_point=start_point, end_point=end_point, width=4)
+for x in range(50, 70, 5):
+    color = sd.random_color()
+    x += 5
+    start_point = sd.get_point(x, 50)
+    for x1 in range(350, 370, 5):
+        x1 += 5
+        end_point = sd.get_point(x1, 450)
+        sd.line(start_point=start_point, end_point=end_point, width=4)
+
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # TODO здесь ваш код
