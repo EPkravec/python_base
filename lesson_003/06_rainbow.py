dfba7725242a47ff36c5534b52c7d3cc7a302a3c
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-05-23 21:59:04 +0400

    вот теперь правильно сделал радугу, ранее было не правильно так как построение линий было не корректным по енд поинт

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -5,25 +5,24 @@
 import simple_draw as sd
 
 sd.resolution = (1200, 600)
-sd.background_color = (0, 0, 255)
+sd.background_color = (255, 255, 255)
 sd.caption = 'Радуга дуга 1 и 2 варианты'
-
+rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN,
+                      sd.COLOR_CYAN, sd.COLOR_BLUE, sd.COLOR_PURPLE)
 
 x, y = 50, 50
 x1, y1 = 350, 450
-
-start_point = sd.get_point(x, y)
-end_point = sd.get_point(x1, y1)
-sd.line(start_point=start_point, end_point=end_point, width=4)
-for x in range(50, 70, 5):
-    color = sd.random_color()
-    x += 5
-    start_point = sd.get_point(x, 50)
-    for x1 in range(350, 370, 5):
-        x1 += 5
-        end_point = sd.get_point(x1, 450)
-        sd.line(start_point=start_point, end_point=end_point, width=4)
-
+step = (5 + 4) * 6 + 1
+rang_start = x + step
+i = 0
+for x in range(50, rang_start, 9):
+    x += 9
+    x1 += 9
+    start_point = sd.get_point(x, y)
+    end_point = sd.get_point(x1, y1)
+    colors = rainbow_colors[i]
+    i += 1
+    sd.line(start_point=start_point, end_point=end_point, color=colors, width=4)
 
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 # TODO здесь ваш код
