3e9698f3194a08e4e834ca3aea59da7af4b30f99
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-05-27 17:49:10 +0400

    наложение выходит и хоть ты тресни ! не  могу понять почему

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,31 +1,39 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
-import row as row
+# noinspection PyUnresolvedReferences
 import simple_draw as sd
 
 sd.resolution = (600, 600)
 sd.background_color = (0, 0, 255)
 sd.caption = 'Стена'
+
 start_left_point_x, start_left_point_y = x, y, = 0, 0
 end_right_point_x, end_right_point_y = x1, y1 = 100, 50
 
-for y in range(-51, 700, 50):
-    y += 50
-    y1 += 50
-    for x in range(-101, 700, 100):
+step = 600
+# todo  не пойму почему наложение происходит ?  степ это переменная для определения количества строк, и цикл под нее
+# todo и условие поставил  помоги !!!
+for step in range(12):
+    if step / 50 % 2 == 0:
         x += 100
         x1 += 100
-        left_point = sd.get_point(x, y)
-        right_point = sd.get_point(x1, y1)
-        sd.rectangle(left_bottom=left_point, right_top=right_point, width=2)
-
-
-
-#   цикл по координате Y
-#       вычисляем сдвиг ряда кирпичей
-#       цикл координате X
-#           вычисляем правый нижний и левый верхний углы кирпича
-#           рисуем кирпич
+        y += 50
+        y1 += 50
+        for x in range(-101, 700, 50):
+            for y in range(-51, 700, 50):
+                left_point = sd.get_point(x, y)
+                right_point = sd.get_point(x1, y1)
+                sd.rectangle(left_bottom=left_point, right_top=right_point, color=sd.COLOR_DARK_GREEN, width=2)
+    else:
+        for x in range(-101, 700, 50):
+            x += 50
+            x1 += 50
+            for y in range(-51, 700, 50):
+                y += 0
+                y1 += 0
+                left_point = sd.get_point(x, y)
+                right_point = sd.get_point(x1, y1)
+                sd.rectangle(left_bottom=left_point, right_top=right_point, color=sd.COLOR_BLACK, width=2)
 
 sd.pause()
