6c1964eb0f03c57acce889b76fcee59fbcfce8e5
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-05-27 15:12:30 +0400

    добавление row пока без успешно

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -2,24 +2,26 @@
 
 # (цикл for)
 import row as row
-
 import simple_draw as sd
+
 sd.resolution = (600, 600)
 sd.background_color = (0, 0, 255)
 sd.caption = 'Стена'
 start_left_point_x, start_left_point_y = x, y, = 0, 0
 end_right_point_x, end_right_point_y = x1, y1 = 100, 50
-for x in range(-101, 700, 100):
-    x += 100
-    x1 += 100
-    # TODO тут необходимо добавить сдвиг туда-сюда, чтобы было похоже на кирпичную кладку, а не просто сетка
-    for y in range(-51, 700, 50):
-        y += 50
-        y1 += 50
+
+for y in range(-51, 700, 50):
+    y += 50
+    y1 += 50
+    for x in range(-101, 700, 100):
+        x += 100
+        x1 += 100
         left_point = sd.get_point(x, y)
         right_point = sd.get_point(x1, y1)
         sd.rectangle(left_bottom=left_point, right_top=right_point, width=2)
-#
+
+
+
 #   цикл по координате Y
 #       вычисляем сдвиг ряда кирпичей
 #       цикл координате X
