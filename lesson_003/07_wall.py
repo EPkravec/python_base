64bb4b6f2609bf76b188870a81a4465f5cc292a2
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-05-27 14:56:20 +0400

    тодо найминг - сделал

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -1,31 +1,24 @@
 # -*- coding: utf-8 -*-
 
 # (цикл for)
+import row as row
+
 import simple_draw as sd
 sd.resolution = (600, 600)
 sd.background_color = (0, 0, 255)
 sd.caption = 'Стена'
-# TODO тут тоже поправь нейминг, типа левая нижняя точка, правая верхняя, или координата по х кирпича... как-то так
-x, y, = 0, 0
-x1, y1 = 100, 50
+start_left_point_x, start_left_point_y = x, y, = 0, 0
+end_right_point_x, end_right_point_y = x1, y1 = 100, 50
 for x in range(-101, 700, 100):
-    # TODO тут необходимо добавить сдвиг туда-сюда, чтобы было похоже на кирпичную кладку, а не просто сетка
     x += 100
     x1 += 100
+    # TODO тут необходимо добавить сдвиг туда-сюда, чтобы было похоже на кирпичную кладку, а не просто сетка
     for y in range(-51, 700, 50):
         y += 50
         y1 += 50
         left_point = sd.get_point(x, y)
         right_point = sd.get_point(x1, y1)
         sd.rectangle(left_bottom=left_point, right_top=right_point, width=2)
-
-# Нарисовать стену из кирпичей. Размер кирпича - 100х50
-# Использовать вложенные циклы for
-
-# TODO здесь ваш код
-# Подсказки:
-#  Для отрисовки кирпича использовать функцию rectangle
-#  Алгоритм должен получиться приблизительно такой:
 #
 #   цикл по координате Y
 #       вычисляем сдвиг ряда кирпичей
