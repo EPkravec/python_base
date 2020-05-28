c230d5fdab0b67d968676076bef3fb6178fc06be
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-05-28 13:21:11 +0400

    тодо сделал

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -9,33 +9,53 @@
 # Формат вывода:
 #   Студенту надо попросить ХХХ.ХХ рублей
 
-educational_grant, expenses = 10000, 12000
-# TODO это надо делать в цикле.
-expenses_1 = expenses + expenses * 0.03
-expenses_2 = expenses + expenses_1 * 0.03
-expenses_3 = expenses + expenses_2 * 0.03
-expenses_4 = expenses + expenses_3 * 0.03
-expenses_5 = expenses + expenses_4 * 0.03
-expenses_6 = expenses + expenses_5 * 0.03
-expenses_7 = expenses + expenses_6 * 0.03
-expenses_8 = expenses + expenses_7 * 0.03
-expenses_9 = expenses + expenses_8 * 0.03
-total_expenses = expenses + expenses_1 + expenses_2 + expenses_3 + expenses_4 + expenses_5 + expenses_6 + expenses_7 + expenses_8 + expenses_9
-total_education = educational_grant * 10
-money = total_expenses - total_education
-print('1 вариант для понимания процесса')
-print('Студенту надо попросить', int(money), 'рублей')
+# educational_grant, expenses = 10000, 12000
+# expenses_1 = expenses + expenses * 0.03
+# expenses_2 = expenses + expenses_1 * 0.03
+# expenses_3 = expenses + expenses_2 * 0.03
+# expenses_4 = expenses + expenses_3 * 0.03
+# expenses_5 = expenses + expenses_4 * 0.03
+# expenses_6 = expenses + expenses_5 * 0.03
+# expenses_7 = expenses + expenses_6 * 0.03
+# expenses_8 = expenses + expenses_7 * 0.03
+# expenses_9 = expenses + expenses_8 * 0.03
+# total_expenses = expenses + expenses_1 + expenses_2 + expenses_3 + expenses_4 + expenses_5 + expenses_6 + expenses_7 + expenses_8 + expenses_9
+# total_education = educational_grant * 10
+# money = total_expenses - total_education
+# print('1 вариант для понимания процесса')
+# print('Студенту надо попросить', int(money), 'рублей')
+#
+# procent = 0.03
+# monht = 0
+# for monht in range(10):
+#     monht += 1
+#     total_money_monht = expenses + educational_grant + procent * expenses
+#     rasxod = expenses - educational_grant
+#     money_1 = monht * rasxod + rasxod * procent
+#     print('Студенту надо попросить', money_1, 'рублей')
 
-print('2 вариант для цикла')
+
 procent = 0.03
-# TODO нейминг, тут наверное счетчик месяцев мог бы быть.
-i = 1
-while i < 10:
-    i += 1
-    # TODO не совсем понял, зачем эта проверка...
-    if i in range(11):
-        #  TODO нет, тебе надо в цикле высчитывать текущую стоимость текущего
-        #   мес и из неё вычитать стипендию и все это скидывать в тотал
-        rasxod = expenses - educational_grant
-        money_1 = int(i * rasxod + rasxod * procent)
-        print('Студенту надо попросить', money_1, 'рублей')
+educational_grant, expenses = 10000, 12000
+rasxodu = expenses - educational_grant
+monht = 1
+for monht in range(9):
+    monht += 1
+    if monht == 1:
+        continue
+    if monht == 2:
+        continue
+    if monht == 3:
+        continue
+    if monht == 4:
+        continue
+    if monht == 5:
+        continue
+    if monht == 6:
+        continue
+    if monht == 7:
+        continue
+    if monht == 8:
+        continue
+    need_money_mam = monht * (rasxodu + (rasxodu * 9) * procent)
+    print('Студенту надо попросить', need_money_mam, 'рублей')
