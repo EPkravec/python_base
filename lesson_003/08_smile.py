ba795fbcac01175a9a8a057eddff580e0507fcef
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-06-20 21:59:00 +0400

    почему не получаеться с листом не пойму в каком формате вводить

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -1,13 +1,31 @@
 # -*- coding: utf-8 -*-
 
 # (определение функций)
-import simple_draw
+import simple_draw as sd
+
+sd.resolution = (700, 700)
+sd.background_color = (0, 0, 255)
+sd.caption = 'Смайлики'
+
+coordinat_face = sd.get_point(350, 350)
+radius_face = 40
+sd.circle(center_position=coordinat_face, radius=radius_face)
+
+coordinat_okko_left = sd.get_point(335, 360)
+radius__okko_left = 5
+sd.circle(center_position=coordinat_okko_left, radius=radius__okko_left)
+
+coordinat_okko_right = sd.get_point(365, 360)
+radius__okko_right = 5
+sd.circle(center_position=coordinat_okko_right, radius=radius__okko_right)
+
+my_list = [335, 330, 365, 330]
+sd.lines(point_list=my_list)
 
 # Написать функцию отрисовки смайлика по заданным координатам
 # Форма рожицы-смайлика на ваше усмотрение
 # Параметры функции: кордината X, координата Y, цвет.
 # Вывести 10 смайликов в произвольных точках экрана.
-
 # TODO здесь ваш код
 
-simple_draw.pause()
+sd.pause()
