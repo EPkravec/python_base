c6f9c8b0f7ef69772a57fe986f3aff76f98c6bd5
Кравец Егор Павлович <kravec.egor@gmail.com>
2020-07-21 15:16:01 +0400

    это не студент а ходячая ипотека, сделал прошу проверить

--- lesson_003/04_student.py
+++ lesson_003/04_student.py
@@ -1,23 +1,14 @@
 # -*- coding: utf-8 -*-
+procent = 1.03
+educational_grant = 10000
+expenses = 12000
+expenses_one_month = 12000
 
-
-procent = 0.03
-educational_grant, expenses = 10000, 12000
-month_no_procent = 1
+total_months = 10
 months_with_interest = 9
-months_school = 10
 
-annual_educational_grant = educational_grant * months_school
-# TODO почему у тебя переменная, которая итерируется - percentage_of_expenses?
-#  Это же должен быть счетчик. Тебе в каждой итериции надо наращивать expenses на * 1.03 и
-#  от этого числа отнимать степендию, и результат наращивать в need_money уже.
-#  Попробуй, если не получится, я подскажу кодом.
-for percentage_of_expenses in range(10):
-    percentage_of_expenses += (expenses + expenses * procent) * months_with_interest
-    percentage_of_expenses_total = expenses + percentage_of_expenses
-    need_money = percentage_of_expenses_total - annual_educational_grant
-
-
+for _ in range(months_with_interest):
+    expenses = expenses * procent
+    expenses = expenses + expenses_one_month
+    need_money = expenses - educational_grant * total_months
 print('Студенту надо попросить', int(need_money), 'рублей')
-
-
