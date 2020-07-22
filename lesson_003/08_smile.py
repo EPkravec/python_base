5f2a608592e18f27daa6d6bc95f8368969a7126c
EPkravec <kravec.egor@gmail.com>
2020-07-22 15:44:36 +0400

    тодо

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -32,10 +32,34 @@ sd.sleep(3)
 sd.clear_screen()  # 10 смайликов рандомно
 
 
-def smail(coordinat_face, coordinat_okko_left, coordinat_okko_right, my_list_right_cheek, my_list_left_cheek,
-          my_list_center_cheek):
+def smail(point):
+    radius_face = 40
+    coordinat_face = sd.get_point(350, 350)
+    coordinat_okko_left = sd.get_point(335, 360)
+    radius__okko_left = 5
+    coordinat_okko_right = sd.get_point(365, 360)
+    radius__okko_right = 5
+    my_list_right_cheek = [sd.get_point(375, 340), sd.get_point(360, 330)]
+    my_list_left_cheek = [sd.get_point(325, 340), sd.get_point(340, 330)]
+    my_list_center_cheek = [sd.get_point(360, 330), sd.get_point(340, 330)]
+    for _ in range(10):
+        sd.circle(center_position=coordinat_face, radius=radius_face)
+        sd.circle(center_position=coordinat_okko_left, radius=radius__okko_left)
+        sd.circle(center_position=coordinat_okko_right, radius=radius__okko_right)
+        sd.lines(point_list=my_list_right_cheek)
+        sd.lines(point_list=my_list_left_cheek)
+        sd.lines(point_list=my_list_center_cheek)
 
-# todo дальше пока не пойму, что нужно сделать и как цикл будет производить привязку всех частей смайла, или как
-#  их сгруппировать в один смайл, что бы можно было циклить его, а не его составные части.
-#  Оберни отрисовку целого смайла в ф-ию, и вызывай эту ф-ию, на вход координаты отрисовки.
+
+for _ in range(10):
+    point = sd.random_point()
+    smail(point=point)
+    sd.circle(center_position=coordinat_face, radius=radius_face)
+    sd.circle(center_position=coordinat_okko_left, radius=radius__okko_left)
+    sd.circle(center_position=coordinat_okko_right, radius=radius__okko_right)
+    sd.lines(point_list=my_list_right_cheek)
+    sd.lines(point_list=my_list_left_cheek)
+    sd.lines(point_list=my_list_center_cheek)
+
 sd.pause()
+# todo  чет не так %) возможно в def smail  нужно еще переменные ? или принципиально нето?
