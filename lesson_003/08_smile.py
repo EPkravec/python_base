ac28c1e3cdf25088f309d29c53fc28da7c9db58c
EPkravec <kravec.egor@gmail.com>
2020-07-22 19:59:04 +0400

    пробую через х и у  внести изменения

--- lesson_003/08_smile.py
+++ lesson_003/08_smile.py
@@ -35,13 +35,15 @@ sd.clear_screen()  # 10 смайликов рандомно
 def smail(point):
     radius_face = 40
     coordinat_face = sd.get_point(350, 350)
-    coordinat_okko_left = sd.get_point(335, 360)
+
     radius__okko_left = 5
-    coordinat_okko_right = sd.get_point(365, 360)
+
     radius__okko_right = 5
-    my_list_right_cheek = [sd.get_point(375, 340), sd.get_point(360, 330)]
-    my_list_left_cheek = [sd.get_point(325, 340), sd.get_point(340, 330)]
-    my_list_center_cheek = [sd.get_point(360, 330), sd.get_point(340, 330)]
+    coordinat_okko_left = sd.get_point(x - 15, y + 10)
+    coordinat_okko_right = sd.get_point(x + 15, y + 10)
+    my_list_right_cheek = [sd.get_point(x + 25, y - 10), sd.get_point(x + 10, y - 20)]
+    my_list_left_cheek = [sd.get_point(x - 25, y - 10), sd.get_point(x - 10, y - 20)]
+    my_list_center_cheek = [sd.get_point(x + 10, y - 20), sd.get_point(x - 10, y - 20)]
     for _ in range(10):
         sd.circle(center_position=coordinat_face, radius=radius_face)
         sd.circle(center_position=coordinat_okko_left, radius=radius__okko_left)
@@ -54,7 +56,7 @@ def smail(point):
 for _ in range(10):
     point = sd.random_point()
     smail(point=point)
-    sd.circle(center_position=coordinat_face, radius=radius_face)
+    sd.circle(center_position=point, radius=radius_face)
     sd.circle(center_position=coordinat_okko_left, radius=radius__okko_left)
     sd.circle(center_position=coordinat_okko_right, radius=radius__okko_right)
     sd.lines(point_list=my_list_right_cheek)
