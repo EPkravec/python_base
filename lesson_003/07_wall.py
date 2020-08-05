c1d40f54f1be0bab8941b60024a2fcdafc08a440
Trincha Stanislav <stas.trincha@gmail.com>
2020-08-06 00:49:01 +0300

    3-ий модуль - почти зачет

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -14,9 +14,10 @@ for y in range(0, 700, 50):
             left_point = sd.get_point(x, y)
             right_point = sd.get_point(x + 100, y + 50)
             sd.rectangle(left_bottom=left_point, right_top=right_point, width=1)
-    if (y / 50) % 2 == 1:
+    if (y / 50) % 2 == 0:
+        shift = 50
         for x in range(0, 700, 100):
-            left_point = sd.get_point(x - 50, y + 50)
-            right_point = sd.get_point(x + 50, y + 100)
+            left_point = sd.get_point(x + shift, y)
+            right_point = sd.get_point(x + 100 + shift, y + 50)
             sd.rectangle(left_bottom=left_point, right_top=right_point, width=1)
 sd.pause()
