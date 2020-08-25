8016ac28865891a48d219250ac3af5a08732074e
Максим <bi4ok23@gmail.com>
2020-08-25 20:37:43 +0300

    lesson 003: зачёт

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -8,13 +8,21 @@ sd.caption = 'Стена'
 
 x, y, = 0, 0
 
-for y in range(0, 700, 50):
-    if (y / 50) % 2 == 1:
-        shift = 0
-    else:
-        shift = 50
+# for y in range(0, 700, 50):
+#     if (y / 50) % 2 == 1:
+#         shift = 0
+#     else:
+#         shift = 50
+#     for x in range(shift, 700, 100):
+#         left_point = sd.get_point(x, y)
+#         right_point = sd.get_point(x + 100, y + 50)
+#         sd.rectangle(left_bottom=left_point, right_top=right_point, width=1)
+
+
+for row, y in enumerate(range(0, 700, 50)):
+    shift = 0 if row % 2 else -50
     for x in range(shift, 700, 100):
-        left_point = sd.get_point(x, y)
-        right_point = sd.get_point(x + 100, y + 50)
-        sd.rectangle(left_bottom=left_point, right_top=right_point, width=1)
+        sd.rectangle(left_bottom=sd.get_point(x, y), right_top=sd.get_point(x + 100, y + 50), width=1)
 sd.pause()
+
+#зачёт!
