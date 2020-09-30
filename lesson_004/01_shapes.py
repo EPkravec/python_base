1ab3a667c8e27a9ae36cb8a1d9405853933eb4d4
EPkravec <kravec.egor@gmail.com>
2020-09-30 16:00:15 +0400

    прошу проверить, вектора не соедины в конце

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -5,81 +5,111 @@ import simple_draw as sd
 sd.resolution = (600, 600)
 
 
-def triangle(point, angle, length, delta):
-    while angle > 360:
-        return
+def triangle(point, angle, length):
     v1 = sd.get_vector(start_point=point, angle=angle, length=length)
     v1.draw()
-    next_angle_triangle = angle + delta_triangle
-    next_point = v1.end_point
-    # TODO сделай пожалуйста в цикле, использовать рекурсию, где в этом нет особой необходимости - не надо, как минимум усложняет чтение кода.
-    triangle(point=next_point, angle=next_angle_triangle, length=length, delta=delta)
+    for next_angle in range(0, 181, delta_triangle):
+        next_point = v1.end_point
+        next_angle += angle + delta_triangle
+        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+        v1.draw()
 
 
-def square(point, angle, length, delta):
-    while angle > 360:
-        return
+def square(point, angle, length):
     v1 = sd.get_vector(start_point=point, angle=angle, length=length)
     v1.draw()
-    next_angle_square = angle + delta_square
-    next_point = v1.end_point
-    square(point=next_point, angle=next_angle_square, length=length, delta=delta)
+    for next_angle in range(0, 181, delta_square):
+        next_point = v1.end_point
+        next_angle += angle + delta_square
+        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+        v1.draw()
 
 
-def pentagon(point, angle, length, delta):
-    while angle > 360:
-        return
+def pentagon(point, angle, length):
     v1 = sd.get_vector(start_point=point, angle=angle, length=length)
     v1.draw()
-    next_angle_pentagon = angle + delta_pentagon
-    next_point = v1.end_point
-    pentagon(point=next_point, angle=next_angle_pentagon, length=length, delta=delta)
+    for next_angle in range(0, 271, delta_pentagon):
+        next_point = v1.end_point
+        next_angle += angle + delta_pentagon
+        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+        v1.draw()
 
 
-def hexagon(point, angle, length, delta):
-    while angle > 360:
-        return
+def hexagon(point, angle, length):
     v1 = sd.get_vector(start_point=point, angle=angle, length=length)
     v1.draw()
-    next_angle_hexagon = angle + delta_hexagon
-    next_point = v1.end_point
-    hexagon(point=next_point, angle=next_angle_hexagon, length=length, delta=delta)
+    for next_angle in range(0, 271, delta_hexagon):
+        next_point = v1.end_point
+        next_angle += angle + delta_hexagon
+        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+        v1.draw()
 
 
-point_triangle = sd.get_point(100, 50)
 angle = 30
 length = 100
 delta_triangle = 120
 delta_square = 90
 delta_pentagon = 72
 delta_hexagon = 60
+point_triangle = sd.get_point(100, 50)
 point_square = sd.get_point(400, 50)
 point_pentagon = sd.get_point(400, 400)
 point_hexagon = sd.get_point(100, 400)
 
-
-triangle(point=point_triangle, angle=angle, length=length, delta=delta_triangle)
-square(point=point_square, angle=angle, length=length, delta=delta_square)
-pentagon(point=point_pentagon, angle=angle, length=length, delta=delta_pentagon)
-hexagon(point=point_hexagon, angle=angle, length=length, delta=delta_hexagon)
+triangle(point=point_triangle, angle=angle, length=length)
+square(point=point_square, angle=angle, length=length)
+pentagon(point=point_pentagon, angle=angle, length=length)
+hexagon(point=point_hexagon, angle=angle, length=length)
 
 sd.pause()
 
-
-# # Часть 2 (делается после зачета первой части)
-# #
-# # Надо сформировать функцию, параметризированную в местах где была "небольшая правка".
-# # Это называется "Выделить общую часть алгоритма в отдельную функцию"
-# Потом надо изменить функции рисования конкретных фигур - вызывать общую функцию вместо "почти" одинакового кода.
+# todo  попробовал сделать вот такую ф-ю но к сожалению пока не могу выполнить один критерий ( аргумент в ф-и point)
+# todo нужно использовать словарь или список точек для подстановки только пока как реализовать не понимаю
+# todo
+# def figures(angle, length):
+#     v3 = sd.get_vector(start_point=point_triangle, angle=angle, length=length)
+#     v3.draw()
+#     v4 = sd.get_vector(start_point=point_square, angle=angle, length=length)
+#     v4.draw()
+#     v5 = sd.get_vector(start_point=point_pentagon, angle=angle, length=length)
+#     v5.draw()
+#     v6 = sd.get_vector(start_point=point_hexagon, angle=angle, length=length)
+#     v6.draw()
+#     for next_angle in range(0, 181, delta_triangle):
+#         next_point = v3.end_point
+#         next_angle += angle + delta_triangle
+#         v3 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+#         v3.draw()
+#     for next_angle in range(0, 181, delta_square):
+#         next_point = v4.end_point
+#         next_angle += angle + delta_square
+#         v4 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+#         v4.draw()
+#     for next_angle in range(0, 271, delta_pentagon):
+#         next_point = v5.end_point
+#         next_angle += angle + delta_pentagon
+#         v5 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+#         v5.draw()
+#     for next_angle in range(0, 271, delta_hexagon):
+#         next_point = v6.end_point
+#         next_angle += angle + delta_hexagon
+#         v6 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+#         v6.draw()
 #
-# В итоге должно получиться:
-#   - одна общая функция со множеством параметров,
-#   - все функции отрисовки треугольника/квадрата/етс берут 3 параметра и внутри себя ВЫЗЫВАЮТ общую функцию.
+# angle = 30
+# length = 100
+# delta_triangle = 120
+# delta_square = 90
+# delta_pentagon = 72
+# delta_hexagon = 60
 #
-# Не забудте в этой общей функции придумать, как устранить разрыв в начальной/конечной точках рисуемой фигуры
-# (если он есть. подсказка - на последней итерации можно использовать линию от первой точки)
-
-# Часть 2-бис.
-# А теперь - сколько надо работы что бы внести изменения в код? Выгода на лицо :)
-# Поэтому среди программистов есть принцип D.R.Y. https://clck.ru/GEsA9
-# Будьте ленивыми, не используйте копи-пасту!
+# point_triangle = sd.get_point(100, 50)
+# point_square = sd.get_point(400, 50)
+# point_pentagon = sd.get_point(400, 400)
+# point_hexagon = sd.get_point(100, 400)
+#
+#
+# figures(angle=angle, length=length)
+#
+#
+# sd.pause()
