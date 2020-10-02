36fe3ba3a39fc860657a8d26a7008c89afef428e
EPkravec <kravec.egor@gmail.com>
2020-10-02 11:08:40 +0400

    прошу проверить

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -1,14 +1,37 @@
 # -*- coding: utf-8 -*-
-
 import simple_draw as sd
 
-# Запросить у пользователя желаемую фигуру посредством выбора из существующих
-#   вывести список всех фигур с номерами и ждать ввода номера желаемой фигуры.
-# и нарисовать эту фигуру в центре экрана
+sd.resolution = (600, 600)
+color_selection = ['0 : треугольник', '1 : квадрат', '2 : пятиугольник', '3 : шестиугольник']
 
-# Код функций из упр lesson_004/02_global_color.py скопировать сюда
-# Результат решения см lesson_004/results/exercise_03_shape_select.jpg
+print('Возможные цвета:', sep='\n', *color_selection)
+user_data = int(input('Введите желаемый цвет > '))
+if user_data == 0:
+    delta = 120
+elif user_data == 1:
+    delta = 90
+elif user_data == 2:
+    delta = 72
+elif user_data == 3:
+    delta = 60
+else:
+    print('Вы ввели не корректный номер')
 
-# TODO здесь ваш код
+
+def figur(point, angle, length):
+    v1 = sd.get_vector(start_point=point, angle=angle, length=length)
+    v1.draw()
+    for next_angle in range(0, 271, delta):
+        next_point = v1.end_point
+        next_angle += angle + delta
+        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+        v1.draw()
+
+
+angle = 30
+length = 100
+point = sd.get_point(300, 300)
+
+figur(point=point, angle=angle, length=length)
 
 sd.pause()
