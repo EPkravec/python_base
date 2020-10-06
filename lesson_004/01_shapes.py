daa0cb88aa730a9aae857a585efd4fd4d4e8685c
EPkravec <kravec.egor@gmail.com>
2020-10-06 19:04:03 +0400

    получаеться только так, с дельтой, что нужно сделать если не верно

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -1,72 +1,50 @@
 # -*- coding: utf-8 -*-
 
 import simple_draw as sd
-import random
 
 sd.resolution = (600, 600)
 
-# TODO не совсем так), тебе надо сделать базовую ф-ию, выглядеть она будет как triangle, например, а сама triangle внутри будет только одной строкой вызывать юту ф-ию с нужными параметрами, все остальные ф-ии - также)
-def figur(point, angle, length):
-    triangle(point=point_triangle, angle=angle, length=length)
-    square(point=point_square, angle=angle, length=length)
-    pentagon(point=point_pentagon, angle=angle, length=length)
-    hexagon(point=point_hexagon, angle=angle, length=length)
+
+def figur(start_point, angle, length):
+    v1 = sd.get_vector(start_point=start_point, angle=angle, length=length)
+    v1.draw()
+    for next_angle in range(0, 271, delta):
+        next_point = v1.end_point
+        next_angle += angle + delta
+        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
+        v1.draw()
 
 
 def triangle(point, angle, length):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length)
-    v1.draw()
-    for next_angle in range(0, 181, delta_triangle):
-        next_point = v1.end_point
-        next_angle += angle + delta_triangle
-        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
-        v1.draw()
+    figur(start_point=point, angle=angle, length=length)
 
 
 def square(point, angle, length):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length)
-    v1.draw()
-    for next_angle in range(0, 181, delta_square):
-        next_point = v1.end_point
-        next_angle += angle + delta_square
-        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
-        v1.draw()
+    figur(start_point=point, angle=angle, length=length)
 
 
 def pentagon(point, angle, length):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length)
-    v1.draw()
-    for next_angle in range(0, 271, delta_pentagon):
-        next_point = v1.end_point
-        next_angle += angle + delta_pentagon
-        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
-        v1.draw()
+    figur(start_point=point, angle=angle, length=length)
 
 
 def hexagon(point, angle, length):
-    v1 = sd.get_vector(start_point=point, angle=angle, length=length)
-    v1.draw()
-    for next_angle in range(0, 271, delta_hexagon):
-        next_point = v1.end_point
-        next_angle += angle + delta_hexagon
-        v1 = sd.get_vector(start_point=next_point, angle=next_angle, length=length)
-        v1.draw()
+    figur(start_point=point, angle=angle, length=length)
 
 
 angle = 30
 length = 100
-delta_triangle = 120
-delta_square = 90
-delta_pentagon = 72
-delta_hexagon = 60
-x = random.randint(0, 20)
-y = random.randint(0, 20)
-point_0 = sd.get_point(x, y)
-point_triangle = sd.get_point(x + 100, y + 50)
-point_square = sd.get_point(x + 400, y + 50)
-point_pentagon = sd.get_point(x + 400, y + 400)
-point_hexagon = sd.get_point(x + 100, y + 400)
 
-figur(point=point_0, angle=angle, length=length)
+point_triangle = sd.get_point(100, 50)
+point_square = sd.get_point(400, 50)
+point_pentagon = sd.get_point(400, 400)
+point_hexagon = sd.get_point(100, 400)
 
+delta = delta_triangle = 120
+triangle(point=point_triangle, angle=angle, length=length)
+delta = delta_square = 90
+square(point=point_square, angle=angle, length=length)
+delta = delta_pentagon = 72
+pentagon(point=point_pentagon, angle=angle, length=length)
+delta = delta_hexagon = 60
+hexagon(point=point_hexagon, angle=angle, length=length)
 sd.pause()
