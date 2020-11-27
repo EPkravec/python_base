930ac794b62caa9dccd98e2a4303370448232ce3
EPkravec <kravec.egor@gmail.com>
2020-11-27 08:25:02 +0400

    посмотрите!

--- lesson_004/01_shapes.py
+++ lesson_004/01_shapes.py
@@ -5,10 +5,9 @@ import simple_draw as sd
 sd.resolution = (600, 600)
 
 
-def figur(start_point, angle, length):
+def figur(start_point, angle, length, delta):
     v1 = sd.get_vector(start_point=start_point, angle=angle, length=length)
     v1.draw()
-    # TODO я писал в прошлой тудушке, тут непонятно откуда берется дельта, очень не рекомендуется использовать глобальные переменные, константы - можно но переменные - нет
     if delta < 73:
         end_range = 181
     elif delta > 73:
@@ -21,21 +20,20 @@ def figur(start_point, angle, length):
     sd.line(start_point=v1.end_point, end_point=start_point)
 
 
-def triangle(point, angle, length):
-    # TODO в этих функциях надо передавать дельту
-    figur(start_point=point, angle=angle, length=length)
+def triangle(point, angle, length, delta_triangle):
+    figur(start_point=point, angle=angle, length=length, delta=delta_triangle)
 
 
-def square(point, angle, length):
-    figur(start_point=point, angle=angle, length=length)
+def square(point, angle, length, delta_square):
+    figur(start_point=point, angle=angle, length=length, delta=delta_square)
 
 
-def pentagon(point, angle, length):
-    figur(start_point=point, angle=angle, length=length)
+def pentagon(point, angle, length, delta_pentagon):
+    figur(start_point=point, angle=angle, length=length, delta=delta_pentagon)
 
 
-def hexagon(point, angle, length):
-    figur(start_point=point, angle=angle, length=length)
+def hexagon(point, angle, length, delta_hexagon):
+    figur(start_point=point, angle=angle, length=length, delta=delta_hexagon)
 
 
 angle = 30
@@ -46,12 +44,13 @@ point_square = sd.get_point(400, 50)
 point_pentagon = sd.get_point(400, 400)
 point_hexagon = sd.get_point(100, 400)
 
-delta = delta_triangle = 120
-triangle(point=point_triangle, angle=angle, length=length)
-delta = delta_square = 90
-square(point=point_square, angle=angle, length=length)
-delta = delta_pentagon = 72
-pentagon(point=point_pentagon, angle=angle, length=length)
-delta = delta_hexagon = 60
-hexagon(point=point_hexagon, angle=angle, length=length)
+delta_triangle = 120
+delta_square = 90
+delta_pentagon = 72
+delta_hexagon = 60
+
+triangle(point=point_triangle, angle=angle, length=length, delta_triangle=delta_triangle)
+square(point=point_square, angle=angle, length=length, delta_square=delta_square)
+pentagon(point=point_pentagon, angle=angle, length=length, delta_pentagon=delta_pentagon)
+hexagon(point=point_hexagon, angle=angle, length=length, delta_hexagon=delta_hexagon)
 sd.pause()
