9a0f5d70caa22af01237f81d82fb0346826a5c4c
strincha <strincha@uma.tech>
2020-12-05 01:46:23 +0300

    5-ый модуль - не зачет

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import room_1, room_2
 
-
+# TODO соедини через join, чтобы жильцы были через запятую
 print('В комнате room_1 живут:', *room_1.folks)
 print('В комнате room_2 живут:', *room_2.folks)
 
--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -8,7 +8,7 @@ from district.soviet_street.house1 import room1 as f31
 from district.soviet_street.house1 import room2 as f32
 from district.soviet_street.house2 import room1 as f41
 from district.soviet_street.house2 import room2 as f42
-
+# о, тут молодец!
 delimiter = ', '
 people = room_1.folks + room_2.folks
 people_central_street = f11.folks + f12.folks + f21.folks + f22.folks
@@ -17,3 +17,5 @@ people_soviet_street = f31.folks + f32.folks + f41.folks + f42.folks
 print('На неизвестном улице живут:', delimiter.join(people))
 print('На центральной улице живут:', delimiter.join(people_central_street))
 print('На советской улице живут:', delimiter.join(people_soviet_street))
+
+# зачет!
