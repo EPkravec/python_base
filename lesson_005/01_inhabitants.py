257546a1a67c7f210a4aa93e4f794071d56b488b
EPkravec <kravec.egor@gmail.com>
2020-12-09 11:47:02 +0400

    тодо сделал

--- lesson_005/01_inhabitants.py
+++ lesson_005/01_inhabitants.py
@@ -1,7 +1,10 @@
 # -*- coding: utf-8 -*-
 import room_1, room_2
 
-# TODO соедини через join, чтобы жильцы были через запятую
+
 print('В комнате room_1 живут:', *room_1.folks)
 print('В комнате room_2 живут:', *room_2.folks)
 
+delimiter = ', '
+people = room_1.folks + room_2.folks
+print('На неизвестном улице живут:', delimiter.join(people))
