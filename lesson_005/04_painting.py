842d258eb361b7bdd6ec74b19c9fc0e2636481a6
EPkravec <kravec.egor@gmail.com>
2020-12-11 08:37:52 +0400

    добавил дерево

--- lesson_005/04_painting.py
+++ lesson_005/04_painting.py
@@ -1,11 +1,19 @@
-from painting import main_rainbow, main_snowfall
+from painting import main_rainbow, main_snowfall, main_tree
 import simple_draw as sd
 
 
-sd.resolution = (1200, 600)
+sd.resolution = (1400, 600)
 sd.caption = 'Утро в деревне'
 
+angle = 90
+length = 50
+angle_1 = 90
+length_1 = 30
+x, y = 850, 0
+x_1, y_1 = 1000, 20
 
 main_rainbow.rab()
+main_tree.run_tree(angle, length, x, y)
+main_tree.run_tree(angle_1, length_1, x_1, y_1)
 main_snowfall.sno()
 
--- lesson_005/painting/main_tree.py
+++ lesson_005/painting/main_tree.py
@@ -1,19 +1,21 @@
 import simple_draw as sd
 import random
 
-sd.resolution = (900, 800)
-
 
 def draw_branches(point, angle, length):
-    if length < 10:
+    sd.resolution = (1200, 600)
+    if length < 5:
         return
+    color = sd.COLOR_GREEN
     random_angle = random.randint(18, 42)
+    if length > 20:
+        color = sd.COLOR_DARK_ORANGE
     v0 = sd.get_vector(start_point=point, angle=angle, length=length)
-    v0.draw()
+    v0.draw(color=color)
     v1 = sd.get_vector(start_point=v0.end_point, angle=angle + random_angle, length=length)
-    v1.draw()
+    v1.draw(color=color)
     v2 = sd.get_vector(start_point=v0.end_point, angle=angle - random_angle, length=length)
-    v2.draw()
+    v2.draw(color=color)
     random_length = random.uniform(0.6, 0.9)
     next_point_v1 = v1.end_point
     next_point_v2 = v2.end_point
@@ -22,10 +24,12 @@ def draw_branches(point, angle, length):
     draw_branches(point=next_point_v2, angle=angle - random_angle, length=next_length)
 
 
-root_point = sd.get_point(300, 30)
-angle = 90
-length = 100
-draw_branches(point=root_point, angle=angle, length=length)
+def run_tree(angle, length, x, y):
+    root_point = sd.get_point(x, y)
+    angle = angle
+    length = length
+    draw_branches(point=root_point, angle=angle, length=length)
 
-sd.pause()
 
+if __name__ == '__04_painting__':
+    sd.pause()
