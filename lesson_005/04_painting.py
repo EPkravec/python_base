4f9754441933554815f8dfa54fab05ff0c983b50
EPkravec <kravec.egor@gmail.com>
2020-12-11 09:10:35 +0400

    изменил размеры экрана

--- lesson_005/04_painting.py
+++ lesson_005/04_painting.py
@@ -2,7 +2,7 @@ from painting import main_rainbow, main_snowfall, main_tree, main_smile
 import simple_draw as sd
 
 
-sd.resolution = (1400, 600)
+sd.resolution = (1500, 600)
 sd.caption = 'Утро в деревне'
 
 angle = 90
@@ -12,9 +12,11 @@ length_1 = 30
 x, y = 850, 0
 x_1, y_1 = 1000, 20
 
+
 main_rainbow.rab()
 main_smile.run_smail()
 main_tree.run_tree(angle, length, x, y)
 main_tree.run_tree(angle_1, length_1, x_1, y_1)
 main_snowfall.sno()
+
 
