d31925fd613aa437d24f0cc3a2f05ccf620093eb
EPkravec <kravec.egor@gmail.com>
2020-12-23 08:05:05 +0400

    сместил деревья

--- lesson_005/04_painting.py
+++ lesson_005/04_painting.py
@@ -1,5 +1,9 @@
-# TODO лучше импортировать не модули, а что-то конкретное внутри них, например в твоем случае ф-ии
-from painting import main_rainbow, main_snowfall, main_tree, main_smile, main_the_sun, main_homme
+from painting.main_rainbow import rab
+from painting.main_tree import run_tree
+from painting.main_smile import run_smail
+from painting.main_the_sun import sun
+from painting.main_homme import draw_homme
+from painting.main_snowfall import sno
 import simple_draw as sd
 
 
@@ -7,19 +11,19 @@ sd.resolution = (1300, 600)
 sd.caption = 'Утро в деревне'
 
 angle = 90
-length = 50
+length = 20
 angle_1 = 90
 length_1 = 30
 x, y = 850, 0
 x_1, y_1 = 1000, 20
 
 
-main_rainbow.rab()
-main_homme.run_homme()
-main_the_sun.sun()
-main_smile.run_smail()
-main_tree.run_tree(angle, length, x, y)
-main_tree.run_tree(angle_1, length_1, x_1, y_1)
-main_snowfall.sno()
+rab()
+draw_homme()
+sun()
+run_smail()
+run_tree(angle, length, x, y)
+run_tree(angle_1, length_1, x_1, y_1)
+sno()
 
 
