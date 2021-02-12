a03b1e25fcd06790b128d133a321e6007ff1df43
егор <Kravec.egor@gmail.com>
2021-02-12 15:56:57 +0400

    посмотрите. вроде все норм осталось только ссовместить 2 файла

--- lesson_006/01_mastermind.py
+++ lesson_006/01_mastermind.py
@@ -1,8 +1,9 @@
-from mastermind_engine.mastermind_engine import numberComp, numberUS
+from mastermind_engine.mastermind_engine import process
+
 
 print(' Ну что же сыграем в игру ')
 print(' ------------------------ ')
 print(' Введите четырех значное число')
 
-numberComp()
-numberUS()
+number_us = str(input(':'))
+process()
--- lesson_006/mastermind_engine/mastermind_engine.py
+++ lesson_006/mastermind_engine/mastermind_engine.py
@@ -1,71 +1,71 @@
 import random
+from 01_mastermind import number_us  #Todo что за ???????????????????????
 
-# TODO что за название такое ф-ии, все буковки должны быть маленькие и разделитель - нижнее подчеркивание,
-#  ты в java стиле пишешь.
-def numberComp():
-    one_number = random.randint(0, 9)
-    two_number = random.randint(0, 9)
-    fhree_number = random.randint(0, 9)
-    four_number = random.randint(0, 9)
-    # TODO сделай проверку проще, можно преобразовать число к set-у и так понять, что все числа внутри разные, ещё пригодится ф-ия len()
-    #  И у тебя если число не прошло по этим условиям, то ф-ия возвращает None, а по идее она должна дальше генерировать, пока не получится.
-    #  Можно ещё постепенно генерировать число, сначала рандом из всех цифр, потом из всех кроме той, которая уже выпала итд.
-    if one_number != two_number and one_number != fhree_number and one_number != four_number and \
-            two_number != fhree_number and two_number != four_number and \
-            fhree_number != four_number:
-        number_comp = str(one_number) + str(two_number) + str(fhree_number) + str(four_number)
-        print(type(number_comp))
-        return number_comp
 
-# TODO тут тоже название поправь.
-def numberUS():
-    num = 0
-    while True:
-        num += 1
-        # TODO также все взаимодействие с пользователем должнор проходить через главный модуль программы, тут только набор ф-ий.
-        print(f'номер попытки {num}')
-        number_us = str(input(':'))
-        if number_us == 'check':
-            print(numberComp())
-            continue
-        if len(number_us) > 4:
-            print('Должно быть 4 цифры введите повторно')
-            continue
-        if len(number_us) < 4:
-            print('Должно быть 4 цифры введите повторно')
-            continue
-        if number_us[0] != number_us[1] and number_us[0] != number_us[2] and number_us[0] != number_us[3] and \
-                number_us[1] != number_us[2] and number_us[1] != number_us[3] and \
-                number_us[2] != number_us[3]:
-            print('')
-        else:
-            print('Вы ввели одинаковые цифры')
-            continue
+def process():
+    game = True
+    while game == True:
+        number = [random.randint(0, 9) for _ in range(4)]
 
-        nu = list(number_us)
-        nc = list(numberComp()) # TODO у тебя ф-ия numberComp возвращает None
+        if number[0] != number[1] and number[0] != number[2] and number[0] != number[3] and \
+                number[1] != number[2] and number[1] != number[3] and \
+                number[2] != number[3]:
 
-        bulls = 0
-        for j in range(4):
-            if nu[j] == nc[j]:
-                bulls += 1
-                nu[j] = ''
-                nc[j] = '*'
+            number_comp = str(number[0]) + str(number[1]) + str(number[2]) + str(number[3])
 
-        if bulls == 4:
-            print('Вы выграли !')
-            break
+            game = False
+            num = 0
 
-        cows = 0
-        for j in range(4):
-            entered_digit = nc[j]
-            for i in range(4):
-                if entered_digit == nc[i]:
-                    cows += 1
-                    nc[i] = '*'
+            while game == False:
+                num += 1
+                print(f'номер попытки {num}')
+                number_us = str(input(':'))
+                print(number_comp)
+                if number_us == 'check':
+                    print(number_comp)
+                    continue
+                if len(number_us) > 4:
+                    print('Должно быть 4 цифры введите повторно')
+                    continue
+                if len(number_us) < 4:
+                    print('Должно быть 4 цифры введите повторно')
+                    continue
+                if number_us[0] != number_us[1] and number_us[0] != number_us[2] and number_us[0] != number_us[3] and \
+                        number_us[1] != number_us[2] and number_us[1] != number_us[3] and \
+                        number_us[2] != number_us[3]:
+                    print()
+                else:
+                    print('Вы ввели одинаковые цифры')
+                    continue
+
+                nu = list(number_us)
+                nc = list(number_comp)
+
+                bulls = 0
+                for j in range(4):
+                    if nu[j] == nc[j]:
+                        bulls += 1
+                        nu[j] = ''
+                        nc[j] = '*'
+
+                if bulls == 4:
+                    print('Вы выграли !')
                     break
 
-        print(f' быков {bulls}')
-        print(f' коров {cows}')
-
+                cows = 0
+                for j in range(4):
+                    entered_digit = nc[j]
+                    for i in range(4):
+                        if entered_digit == nc[i]:
+                            cows += 1
+                            nc[i] = '*'
+                            break
+                print(f' быков {bulls}')
+                print(f' коров {cows}')
+        else:
+            continue
     return
+
+if __name__ == '__main__':
+    process()
+
