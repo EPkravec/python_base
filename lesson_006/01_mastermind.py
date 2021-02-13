6b9dcc770d44c204990a95bb424be4076df6f4bc
strincha <strincha@uma.tech>
2021-02-13 13:45:21 +0300

    6-ой модуль - не зачет

--- lesson_006/01_mastermind.py
+++
@@ -1,9 +0,0 @@
-from mastermind_engine.mastermind_engine import process
-
-
-print(' Ну что же сыграем в игру ')
-print(' ------------------------ ')
-print(' Введите четырех значное число')
-
-number_us = str(input(':'))
-process()
---
+++ lesson_006/mastermind.py
@@ -0,0 +1,11 @@
+# TODO ты тут импортируешь модуль mastermind_engine, а из него импортируешь этот модуль,
+#  тут циклический импорт получается, старайся такого избегать, пусть number_us у тебя хранится в mastermind_engine
+from mastermind_engine import process
+
+
+print(' Ну что же сыграем в игру ')
+print(' ------------------------ ')
+print(' Введите четырех значное число')
+
+number_us = str(input(':'))
+process()
