406384a2b00e54eca86356b3e5297f71220dbcd7
strincha <strincha@uma.tech>
2021-02-15 03:27:27 +0300

    6-ой модуль - не зачет

--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -1,16 +1,17 @@
 import random
 from mastermind import numberUser
 
-
+# TODO эту ф-ию тебе надо импортировать в mastermind
 def numberComp():
     while True:
-        number_comp = set([random.randint(0, 9) for _ in range(4)])
-        if len(number_comp) > 4 or len(number_comp) < 4:
+        number_comp = set([random.randint(0, 9) for _ in range(4)]) # TODO тубу же надо число генерировать, лучше его в
+                                                                    #  спиок записать, а не в set, а set использовать уже для того, чтобы понять, что в списке 4 разных числа
+        if len(number_comp) > 4 or len(number_comp) < 4: # TODO врядли может быть больше 4-х, тут вообще можно сделать одну проверку, если != 4
             continue
         if len(number_comp) == 4:
             print(number_comp)
             return number_comp
-
+# TODO вот вызов этой ф-ии тоже должен быть в модуле mastermind. Давай пока там импортируй эту ф-ию и вызови генерацию числа
 print(numberComp())
 number_us = numberUser()
 
