4728316926ade5c2a8a52c81601c7702a84623c0
егор <Kravec.egor@gmail.com>
2021-03-04 13:47:26 +0400

    чето не правильно считает коров.  и теперь о взаимедйствии с 1 файлом

--- lesson_006/mastermind.py
+++ lesson_006/mastermind.py
@@ -1,21 +1,12 @@
+from mastermind_engine import *
+
+
 print(' Ну что же сыграем в игру ')
 print(' ------------------------ ')
 print(' Введите четырех значное число')
 
 
-
-def numberUser():
-    num = 0
-    while True:
-        num += 1
-        print(f'номер попытки {num}')
-        number_us = str(input(':'))
-        numb = set(number_us)
-        if len(numb) != 4:
-            print('Должно быть 4 цифры введите повторно')
-            continue
-        if len(numb) == 4:
-            return number_us
-        else:
-            print('Вы ввели одинаковые цифры')
-            continue
+genereatNumber()
+numberUser()
+valid()
+process()
--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -2,6 +2,9 @@ import random
 
 number_comp = []
 number_us = []
+num = 0
+bulls = 0
+cows = 0
 
 
 def genereatNumber():
@@ -10,66 +13,80 @@ def genereatNumber():
         number_comp = [random.randint(0, 9) for _ in range(4)]
         numb_comp_set = set(number_comp)
         if len(numb_comp_set) == 4:
+            number_comp[0] = str(number_comp[0])
+            number_comp[1] = str(number_comp[1])
+            number_comp[2] = str(number_comp[2])
+            number_comp[3] = str(number_comp[3])
             print(f' Сгенерированное число = {number_comp}')
-            return list(number_comp)
+            return number_comp
 
 
 def numberUser():
-    global number_us
-    num = 0
+    global number_us, num
     while True:
         num += 1
         print(f'номер попытки {num}')
-        number_us = input(':')
+        number_us = input('Введите число:')
         numb = set(number_us)
         if len(numb) != 4:
             print('Должно быть 4 цифры введите повторно')
             continue
         if len(numb) == 4:
-            number_us = list(number_us)
             return number_us
         else:
             print('Вы ввели одинаковые цифры')
             continue
 
-nu = list(number_us)
-nc = list(number_comp)
+
+def valid():
+    global number_comp, number_us
+
+    if set(number_comp) == set(number_us):
+        return True
+    else:
+        return False
 
 
+def process():
+    global bulls, cows, num
 
-genereatNumber()
-numberUser()
-print('number_comp', number_comp, type(number_comp))
-print('number_comp items',type(number_comp[0]))
-print('number_us>', number_us, type(number_us))
-print('number_us items >', type(number_us[0]))
+    if valid() == True:
+        print('Вы Выграли')
+    if valid() == False:
 
-print('NEW NC items >', type(nc[0]))
-print('NEW NU items ',type(nu[0]))
+        while True:
+
+            nu = list(number_us)
+            nc = list(number_comp)
+
+            bulls = 0
+            for j in range(4):
+                if nu[j] == nc[j]:
+                    bulls += 1
+                    nu[j] = ''
+                    nc[j] = '*'
+
+            if bulls == 4:
+                print('Вы выграли !')
+                break
+
+            cows = 0
+            for j in range(4):
+                for i in range(4):
+                    if nc[j] == nc[i]:
+                        cows += 1
+                        nc[i] = '*'
+                        break
+
+            print(f' быков {bulls}')
+            print(f' коров {cows}')
+
+            numberUser()
+    return bulls, cows
 
 
-# while True:
-#     nu = number_us
-#     nc = number_comp
-#
-#     bulls = 0
-#     for j in range(4):
-#         if nu[j] == nc[j]:
-#             bulls += 1
-#             nu[j] = ''
-#             nc[j] = '*'
-#
-#     if bulls == 4:
-#         print('Вы выграли !')
-#         break
-#
-#     cows = 0
-#     for j in range(4):
-#         entered_digit = nc[j]
-#         for i in range(4):
-#             if entered_digit == nc[i]:
-#                 cows += 1
-#                 nc[i] = '*'
-#                 break
-#     print(f' быков {bulls}')
-#     print(f' коров {cows}')
+if __name__ == '__main__':
+    genereatNumber()
+    numberUser()
+    valid()
+    process()
