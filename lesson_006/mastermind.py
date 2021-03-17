9547e543447d6596a39748c45baf5aaa537c5488
егор <Kravec.egor@gmail.com>
2021-03-17 08:25:05 +0400

    сделал по тодо

--- lesson_006/mastermind.py
+++ lesson_006/mastermind.py
@@ -1,4 +1,6 @@
-from mastermind_engine import generate_number, valid, process, generate_number, num, number_user
+from mastermind_engine import generate_number, valid, process, generate_number, num
+import pdb
+
 
 print(' Ну что же сыграем в игру ')
 print(' ------------------------ ')
@@ -6,30 +8,18 @@ print(' Введите четырех значное число')
 print(' ------------------------ ')
 
 
-# def number_user():
-#     while True:
-#         global num
-#         num += 1
-#         print(f' Номер попытки {num}')
-#         number_us = str(input(' Введите четырехзначное число:  '))
-#         global number_use
-#         number_use = number_us
-#         return number_use
+pdb.set_trace()
+generate_number()
+number = input("Введите число")
 
-import pdb;pdb.set_trace()
-generate_number() # TODO генерируем число, тут все хорошо
-# TODO далее просим юзера ввести число, в общем то, что ты делаешь в ф-ии number_user
-# то есть просишь пользователя ввести число, затем проверяешь число на валидность valid(number) , затем... ну ок, ты
-# решил не делать функцию, которая проверяет то ли это число, тогда можно вызвать ф-ию process(number) и если быков - 4, 
-# то говоришь, что пользователь отгадал, иначе говоришь, пользователю, сколько быков, сколько коров.
-# И так по кругу.
-number_user()
-valid()
-if valid() == True:
+
+valid(number)
+if valid(number) == True:
     print('Вы выграли')
-process()
-if process() == True:
-    print('Вы выграли')
-else:
-    print(f'Быков {bulls}')
-    print(f'Коров {cows}')
+process(number)
+while True:
+    if bulls == 4:
+        print('Вы выграли')
+    else:
+        print(f'Быков {bulls}')
+        print(f'Коров {cows}')
--- lesson_006/mastermind_engine.py
+++ lesson_006/mastermind_engine.py
@@ -12,76 +12,42 @@ def generate_number():
         if len(set(number_comp_str)) == 4:
             global number_comp
             number_comp = str((''.join(map(str, number_comp_str))))
-            # print(f' Сгенерированное число = {number_comp}')
             return number_comp
-        # TODO этот else писать не надо, т.к. в любом случае цикл переходит на следующую итерацию
-        else:
-            continue
 
-# TODO на самом деле эта ф-ия вообще не нужна, как мне кажедтся, тут похоже просто код, который должен быть в главном модуле и не обязательно его оборачивать в ф-ию.
-# def number_user():
-#     while True:
-#         global num
-#         num += 1
-#         print(f' Номер попытки {num}')
-#         number_us = str(input(' Введите четырехзначное число:  '))
-#         global number_use
-#         number_use = number_us
-#         return number_use, num
 
-# TODO пусть ф-ия принимает строку или список, как тебе удобнее и возвращает значение true если валидное и false
+
 def valid(number):
-    # TODO если это ф-ия, которая проверяет валидность введеного числа, то есть несколько правил, во первых должно быть 4 цифры, во вторых они не должны повторяться
-    global number_comp, number_use
-
-    number_use = list(number_use)
-    number_comp = list(number_comp)
-
     while True:
         if len(number_use) == 4:
             if len(set(number_use)) == len(set(number_comp)):
                 return True
             else:
-                # TODO зачем ты тут вызываешь эту ф-ию, ф-ия valid должна возвращать true или false исё.
-                number_user()
+                return False
 
-        else:
-            number_user()
 
-# TODO эта ф-ия, я так понял возвращает быков и коров, тогда она только это и должна делать, и пусть она принимает
-#  число number и сравнивает его с number_comp, больше эта ф-ия ничего делать не должна.
+
 def process(number):
-    global number_comp, number_use
-
-    number_use = list(number_use)
-    number_comp = list(number_comp)
-
     while True:
-        if valid() == True:
-
             bulls = 0
             cows = 0
 
             for i in range(4):
-                if number_use[i] == number_comp[i]:
+                if number[i] == number_comp[i]:
                     bulls += 1
-                    number_use[i] = ''
+                    number[i] = ''
                 if bulls == 4:
                     return True
 
             for i in number_comp:
-                for j in number_use:
+                for j in number:
                     if i == j:
                         cows += 1
                         break
-            # TODO это тут лишнее
-            number_user()
 
     return bulls, cows
 
 
 if __name__ == '__main__':
     generate_number()
-    # number_user()
-    valid()
-    process()
+    valid(number)
+    process(number)
